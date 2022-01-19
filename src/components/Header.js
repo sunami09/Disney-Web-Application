0cb7733b59896563034b611986ce626156edd9b2
sunami09 <sunamidasgupta@gmail.com>
2022-01-19 17:25:36 +0530

    Added Login Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ const Header = (props) => {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
-
+            <Login>Login</Login>
         </Nav>
         )
 };
@@ -125,6 +125,21 @@ const NavMenu = styled.div`
    @media (max-width: 868px) {
     display: none;
   } 
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
 `;
 
 export default Header;
